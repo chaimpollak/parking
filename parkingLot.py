10a61a3a450e4dd6201ca246c33999921d03dc8b
chaim <cschaimp@gmail.com>
2013-05-22 14:33:44 -0300

    Update parkingLot.py

--- parkingLot.py
+++ parkingLot.py
@@ -1,8 +1,8 @@
 ##########################################################################################
 #
 # This program was created to demonstrate what to do when arriving to a full
-# parking lot with no extra parking space. There are 2 options that you can do,
-# either to sit idle and wait until someone will leave, or to drive around impatiently
+# parking lot with no extra parking space. I have observed 2 options that people do,
+# either they sit idle and wait until someone will leave, or they drive around impatiently
 # like a madman, waiting for someone to leave.
 # This program uses the monte carlo method to simulate an approximation of the time 
 # bieng wasted by either driver and plots them on a graph.
